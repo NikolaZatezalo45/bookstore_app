516e9f6c344f636b00190ca9c97034d24988e4ff
Nikola Zatezalo <nikola@platform45.com>
2019-06-21 11:11:14 +0200

    Forgot Password implemented with letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
-  authenticated :user do
-    root 'pages#home', as: :authenticated_root
+    authenticated :user do
+      root 'pages#home', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
 
-  unauthenticated do
-    root 'devise/sessions#new', as: :unauthenticated_root
-  end
-end
-
+mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 resources :genres
 end
